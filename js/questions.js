437260aaa8a940a87fe4f937240a3f02bd2d9f3e
SAMUEL OLALEKAN <77274476+ola-samuel10@users.noreply.github.com>
2022-09-19 15:09:14 +0100

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,161 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    {
+    numb: 6,
+    question: "the rise and fall of a liquid in tube is",
+    answer: "Capilary",
+    options: [
+      "Viscocity",
+      "Capilary",
+      "surface tension",
+      "motion"
+    ]
+  },
+  {
+    numb: 7,
+    question: "the square of 144 multipy by 2 divided by 8 is ?",
+    answer: "3",
+    options: [
+      "2",
+      "3",
+      "4",
+      "6"
+    ]
+  },
+  {
+    numb: 8,
+    question: "the type of force that oppose relative motion is",
+    answer: "Frictional",
+    options: [
+      "Static",
+      "Cohesion",
+      "adhesion",
+      "Frictional"
+    ]
+  },
+   {
+    numb: 9,
+    question: "the substance that alter the rate of diffusion is called",
+    answer: "Catalyst",
+    options: [
+      "Anaion",
+      "Anode",
+      "Catalyst",
+      "Electolysis"
+    ]
+  },
+  {
+    numb: 11,
+    question: "the longest bone in the body is ?",
+    answer: "Femur",
+    options: [
+      "Spinal cord",
+      "Scapula",
+      "atlas",
+      "Femur"
+    ]
+  },
+  {
+    numb: 12,
+    question: "plant generate there food from sun through?",
+    answer: "Chlorophy",
+    options: [
+      "Miscroscope",
+      "Chlorophy",
+      "wind",
+      "Shadow"
+    ]
+  },
+  {
+    numb: 13,
+    question: "the quatities that have both magnitude and direction is ",
+    answer: "Vector",
+    options: [
+      "Vector",
+      "displacement",
+      "Scalar",
+      "direction"
+    ]
+  },
+  {
+    numb: 14,
+    question: " PPA means",
+    answer: "Place of Primary Assignment",
+    options: [
+      "Position of Primary Assignment",
+      "Place of Primary Assignment",
+      "Personal Prime  Association",
+      "Post Assigntment piorities"
+    ]
+  },
+  {
+    numb: 15,
+    question: "which month comes before june?",
+    answer: "May",
+    options: [
+      "April",
+      "July",
+      "May",
+      "August"
+    ]
+  },
+  
+];
